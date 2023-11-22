0b6e39cd4e36af9f7bbd07d17a09b12f6d10fb6a
Carmen Herrera GDL <carmen.herrera@compucad.com.mx>
2023-11-21 23:59:15 -0600

    ejercicio 1 tarea

--- E1 - TEST SPYDER.py
+++ E1 - TEST SPYDER.py
@@ -7,8 +7,9 @@ This is a temporary script file.
 def multiplica(a,b): 
     variable1=a*2
     variable2=b*3
-    resultado=variable1*variable2
+    resultado=variable1-variable2
     print("Empezando for ")
+    print("Primer cambio, TAREA")
     for i in range(1, 5): 
         print(i)
         resultado=resultado * i
